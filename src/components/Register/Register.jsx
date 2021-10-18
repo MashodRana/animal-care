a4e2294613558dc7547114dbc8e401b049be606b
MashodRana <mashod0rana@gmail.com>
2021-10-18 14:21:46 +0600

    regitrations done.

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,17 +1,46 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router";
+import useFirebase from "../../hooks/useFirebase";
 
 const Register = () => {
+    const [userName, setUserName] = useState("");
     const [userEmail, setUserEmail] = useState("");
-    const [password, setPassword] = useState("");
-
+    const [userPassword, setPassword] = useState("");
+    const {registerWithEmailPassword} = useFirebase();
+    const history = useHistory();
     const handleRegistration = (evnt) => {
         evnt.preventDefault();
+        registerWithEmailPassword(userName, userEmail, userPassword);
+        history.push('/home');
+
+    }
+
+    const handleNameInput = (evnt)=>{
+        setUserName(evnt.target.value);
+    }
+
+    const handleEmailInput = (evnt)=>{
+        setUserEmail(evnt.target.value);
+        console.log(userEmail);
+    }
+
+    const handlePasswordInput = (evnt)=>{
+        setPassword(evnt.target.value);
     }
 
     return (
         <div className="container">
             <div className="d-flex justify-content-center">
                 <form onSubmit={handleRegistration}>
+                <div class="mb-3">
+                        <label class="form-label">Name</label>
+                        <input
+                            type="text"
+                            class="form-control"
+                            name="name"
+                            onChange = {handleNameInput}
+                        />
+                    </div>
                     <div class="mb-3">
                         <label for="exampleInputEmail1" class="form-label">Email address</label>
                         <input
@@ -19,6 +48,8 @@ const Register = () => {
                             class="form-control"
                             id="exampleInputEmail1"
                             name="email"
+                            onChange = {handleEmailInput}
+                            value={userEmail}
                         />
                     </div>
                     <div class="mb-3">
@@ -28,6 +59,8 @@ const Register = () => {
                             class="form-control"
                             id="exampleInputPassword1"
                             name="passowrd"
+                            onChange = {handlePasswordInput}
+                            value={userPassword}
                         />
                     </div>
 
--- src/hooks/useFirebase.jsx
+++ src/hooks/useFirebase.jsx
@@ -1,49 +1,90 @@
 import { useEffect, useState } from "react"
-import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from "firebase/auth";
+import {
+    getAuth, signInWithPopup, GoogleAuthProvider, signOut,
+    onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile
+} from "firebase/auth";
 
 import initializeAuthentication from "../Firebase/firebase.init";
 
 initializeAuthentication();
 
-const useFirebase = ()=>{
+const useFirebase = () => {
     const [user, setUser] = useState({});
     const [error, setError] = useState("");
 
     const auth = getAuth();
     const googleProvider = new GoogleAuthProvider();
 
-    const signInUsingGoogle = ()=>{
+    const signInUsingGoogle = () => {
         signInWithPopup(auth, googleProvider)
-        .then(result=>{
-            const user = result.user;
-            console.log(user);
-            
-        })
-        .catch(error=>{
-            setError(error.message);
-        });
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+
+            })
+            .catch(error => {
+                setError(error.message);
+            });
     };
 
-    const logOut = ()=>{
-        signOut(auth)
-        .then(()=>{
-            setUser({});
+    const registerWithEmailPassword = (name, email, password) => {
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(userCredential => {
+                const user = userCredential.user;
+                console.log("new user credential:", user);
+            })
+            .catch(error => {
+                console.log(error.message);
+            })
+
+        updateProfile(auth.currentUser, {
+            displayName: name
         })
+            .then(() => {
+                // Profile updated!
+                // ...
+            })
+            .catch((error) => {
+                console.log(error.message);
+            });
+    };
+
+    const signInUsingEmailPassword = (email, password) => {
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                console.log(user);
+            })
+            .catch((error) => {
+                console.log(error.message);
+            });
+
+
     }
 
-    useEffect(()=>{
-        onAuthStateChanged(auth, user=>{
-            if(user){
+    const logOut = () => {
+        signOut(auth)
+            .then(() => {
+                setUser({});
+            })
+    }
+
+    useEffect(() => {
+        onAuthStateChanged(auth, user => {
+            if (user) {
                 setUser(user);
             }
         })
-    },[])
+    }, [])
 
     return {
         user,
         error,
         signInUsingGoogle,
         logOut,
+        registerWithEmailPassword,
+        signInUsingEmailPassword
     }
 
 
