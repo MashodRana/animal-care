c997d1c463e0f6fc07da44331516ad0eb3258758
MashodRana <mashod0rana@gmail.com>
2021-10-18 17:16:21 +0600

    class to className

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -31,17 +31,17 @@ const Register = () => {
 
     return (
         <div>
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-                        <div class="card border-0 shadow rounded-3 my-5">
-                            <div class="card-body p-4 p-sm-5">
-                                <h5 class="card-title text-center mb-5 fw-light fs-5">Sign In</h5>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+                        <div className="card border-0 shadow rounded-3 my-5">
+                            <div className="card-body p-4 p-sm-5">
+                                <h5 className="card-title text-center mb-5 fw-light fs-5">Sign In</h5>
                                 <form onSubmit={handleRegistration}>
-                                    <div class="form-floating mb-3">
+                                    <div className="form-floating mb-3">
                                         <input
                                             type="text"
-                                            class="form-control"
+                                            className="form-control"
                                             id="floatingInput"
                                             placeholder="Mashod Rana"
                                             onChange={handleNameInput}
@@ -49,10 +49,10 @@ const Register = () => {
                                         />
                                         <label for="floatingInput">Name</label>
                                     </div>
-                                    <div class="form-floating mb-3">
+                                    <div className="form-floating mb-3">
                                         <input
                                             type="email"
-                                            class="form-control"
+                                            className="form-control"
                                             id="floatingInput"
                                             placeholder="name@example.com"
                                             onChange={handleUserEmailInput}
@@ -60,10 +60,10 @@ const Register = () => {
                                         />
                                         <label for="floatingInput">Email address</label>
                                     </div>
-                                    <div class="form-floating mb-3">
+                                    <div className="form-floating mb-3">
                                         <input
                                             type="password"
-                                            class="form-control"
+                                            className="form-control"
                                             id="floatingPassword"
                                             placeholder="Password"
                                             onChange={handleUserPasswordInput}
@@ -72,24 +72,24 @@ const Register = () => {
                                         <label for="floatingPassword">Password</label>
                                     </div>
 
-                                    {/* <div class="form-check mb-3">
-                                        <input class="form-check-input" type="checkbox" value="" id="rememberPasswordCheck" />
-                                        <label class="form-check-label" for="rememberPasswordCheck">
+                                    {/* <div className="form-check mb-3">
+                                        <input className="form-check-input" type="checkbox" value="" id="rememberPasswordCheck" />
+                                        <label className="form-check-label" for="rememberPasswordCheck">
                                             Remember password
                                         </label>
                                     </div> */}
-                                    <div class="d-grid">
+                                    <div className="d-grid">
                                         <button
-                                            class="btn btn-primary btn-login text-uppercase fw-bold"
+                                            className="btn btn-primary btn-login text-uppercase fw-bold"
                                             type="submit"
 
                                         >Sign in</button>
                                     </div>
                                 </form>
                                 
-                                {/* <div class="d-grid">
-                                    <button class="btn btn-facebook btn-login text-uppercase fw-bold" type="button">
-                                        <i class="fab fa-facebook-f me-2"></i> Sign in with Facebook
+                                {/* <div className="d-grid">
+                                    <button className="btn btn-facebook btn-login text-uppercase fw-bold" type="button">
+                                        <i className="fab fa-facebook-f me-2"></i> Sign in with Facebook
                                     </button>
                                 </div> */}
                                 <p>Already an user? <Link to="/login">Login here</Link> </p>
